624bd88bec3e10fec82257c31a88101ea6411283
Steven Tilley <stilley@kaimaging.com>
2021-04-29 17:20:46 -0400

    Add app files

---
+++ Procfile
@@ -0,0 +1,1 @@
+streamlit run app.py --server.port $PORT --server.headless true --server.enableCORS false
---
+++ app.py
@@ -0,0 +1,264 @@
+import streamlit as st
+import pydicom
+import numpy as np
+from matplotlib import pyplot as plt
+import skimage
+import skimage.feature
+import skimage.transform
+import csv
+
+
+ROI = 250  # mm
+RADIUS = 12.5  # mm
+HOUGH_MIN_DISTANCE = 5  # mm
+CANNY_SIGMA = 1  # mm
+NUM_FEATURES = 10  # mm
+MASK_RADIUS_FRACTION_FOR_ORDERING = 0.5
+MASK_INNER_RADIUS_FRACTION = 0.8
+MASK_OUTER_RADIUS_FRACTION = 1.2
+PMMA_MID_INDEX = 7
+AL_MID_INDEX = 2
+
+@st.cache
+def read_phantom():
+    with open("phantom_spec.txt", "r", newline='') as f:
+        r = csv.reader(f)
+        next(r)  # skip first line
+        phantom = [rt for rt in r]
+        phantom = np.array(phantom).astype(np.float64)
+    return phantom
+
+
+@st.cache
+def hough_wrapper(img, pixel_spacing):
+    edge = skimage.feature.canny(img, sigma=CANNY_SIGMA / pixel_spacing)
+    radius_mid = RADIUS / pixel_spacing
+    radii = np.linspace(radius_mid * 0.9, radius_mid * 1.1, 11)
+    hough_res = skimage.transform.hough_circle(edge, radii)
+    hough_peaks = skimage.transform.hough_circle_peaks(hough_res,
+                                                       radii,
+                                                       total_num_peaks=NUM_FEATURES,
+                                                       min_xdistance=int(HOUGH_MIN_DISTANCE / pixel_spacing),
+                                                       min_ydistance=int(HOUGH_MIN_DISTANCE / pixel_spacing))
+    return np.stack(hough_peaks[1:3], axis=1)
+
+
+def get_means(img, pixel_spacing, centers):
+    ygrid, xgrid = np.mgrid[:img.shape[0], :img.shape[1]]
+    rad = RADIUS / pixel_spacing * MASK_RADIUS_FRACTION_FOR_ORDERING
+    means = [np.mean(img[(ygrid - c[1]) ** 2 + (xgrid - c[0])**2 < rad ** 2])
+             for c in centers]
+    return np.array(means)
+
+
+@st.cache
+def sort_circles(img, pixel_spacing, centers):
+    means = get_means(img, pixel_spacing, centers)
+    theta = np.arctan2(centers[:, 1] - np.mean(centers[:, 1]), centers[:, 0] - np.mean(centers[:, 0]))
+    min_from_center = np.min(np.abs(centers - np.mean(centers, axis=0)), axis=0)
+    if min_from_center[0] > min_from_center[1]:
+        # split vertically
+        group1 = centers[:, 0] < np.mean(centers[:, 0])
+        theta = theta - np.pi / 2
+        theta[theta < -np.pi] += np.pi * 2
+    else:
+        # split horizontally
+        group1 = centers[:, 1] < np.mean(centers[:, 1])
+    group2 = np.logical_not(group1)
+    means1_sorted = means[group1][np.argsort(theta[group1])]
+    means2_sorted = means[group2][np.argsort(theta[group2])]
+    if all(np.diff(means1_sorted) < 0):
+        assert(all(np.diff(means2_sorted) > 0))
+        aluminum = group1
+        pmma = group2
+    elif all(np.diff(means1_sorted) > 0):
+        assert(all(np.diff(means2_sorted) < 0))
+        aluminum = group2
+        pmma = group1
+    else:
+        raise RuntimeError("Unable to determine material")
+    return np.concatenate((centers[aluminum][np.argsort(theta[aluminum])], centers[pmma][np.argsort(theta[pmma])]))
+
+
+@st.cache
+def transform_phantom(centers, phantom, pixel_spacing):
+    phantom = phantom.copy()
+    tradj = skimage.transform.AffineTransform(scale=[pixel_spacing, -pixel_spacing],
+                                              translation=np.mean(centers, axis=0) * np.array([-1, 1]) * pixel_spacing)
+    centers_adj = tradj(centers)
+    tr = skimage.transform.SimilarityTransform()
+    if not tr.estimate(phantom[:, :2], centers_adj[:, :2]):
+        raise RuntimeError("Unable to estimate transform")
+    phantom[:, :2] = tradj.inverse(tr(phantom[:, :2]))
+    radius = RADIUS / pixel_spacing * tr.scale
+    return phantom, radius
+
+
+def plot_circles(img, circle_data, radii=None):
+    mats = {0: "Al", 1: "PMMA"}
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    for i in range(circle_data.shape[0]):
+        ax.plot(circle_data[i, 0], circle_data[i, 1], color='C0', marker='o', linestyle=None)
+        if radii is not None:
+            for radius in radii:
+                ax.add_patch(plt.Circle(circle_data[i, :2], radius=radius, facecolor='none', edgecolor='b'))
+        if circle_data.shape[1] >= 4:
+            ax.text(circle_data[i, 0], circle_data[i, 1] - 5,
+                    f"{circle_data[i, 2]:.2f} {mats[circle_data[i, 3]]}", size=14)
+    return fig
+
+
+@st.cache
+def get_labels(imgshape, circle_data, r1, r2, r3):
+    inner = np.zeros(imgshape, dtype='u8')
+    outer = np.zeros(imgshape, dtype='u8')
+    ygrid, xgrid = np.mgrid[:imgshape[0], :imgshape[1]]
+    for i in range(circle_data.shape[0]):
+        m1 = (xgrid - circle_data[i, 0]) ** 2.0 + (ygrid - circle_data[i, 1]) ** 2 < r1 ** 2
+        if np.any(inner[m1] > 0):
+            raise RuntimeError("Masks overlap")
+        inner[m1] = i + 1
+        m2 = np.logical_xor((xgrid - circle_data[i, 0]) ** 2.0 + (ygrid - circle_data[i, 1]) ** 2 < r2 ** 2,
+                            (xgrid - circle_data[i, 0]) ** 2.0 + (ygrid - circle_data[i, 1]) ** 2 < r3 ** 2)
+        if np.any(outer[m2] > 0):
+            raise RuntimeError("Masks overlap")
+        outer[m2] = i + 1
+    return inner, outer
+
+
+@st.cache
+def get_w(hi, li, minner, mouter):
+    #return np.log(np.mean(hi[mouter]) / np.mean(hi[minner])) / np.log(np.mean(li[mouter]) / np.mean(li[minner]))
+    return np.log(np.mean(li[minner]) / np.mean(li[mouter])) / np.log(np.mean(hi[minner]) / np.mean(hi[mouter]))
+
+
+@st.cache
+def cnr(img, inner, outer, nlabels):
+    cnr = []
+    for i in range(nlabels):
+        im = inner == i + 1
+        om = outer == i + 1
+        cnr.append(np.abs(np.mean(img[im]) - np.mean(img[om])) / np.sqrt(np.var(img[im]) + np.var(img[om])))
+    return np.array(cnr)
+
+
+if __name__ == '__main__':
+    plt.rcParams["image.cmap"] = "Greys_r"
+    high_data = st.sidebar.file_uploader("High energy image file", type=["dcm", "DCM"])
+    low_data = st.sidebar.file_uploader("Low energy image file", type=["dcm", "DCM"])
+    verbose = st.sidebar.checkbox("Verbose")
+    mask_inner_fraction = st.sidebar.text_input("Inner ROI radius fraction",
+                                                value=0.8,
+                                                help="Multiply the feature radius by this value to get the radius "
+                                                     "of the inner mask.")
+    mask_outer_fraction = st.sidebar.text_input("Outer ROI radius fraction",
+                                                value=1.2,
+                                                help="Multiply the feature radius by this value to get the inner "
+                                                     "radius of the outer mask. The outer radius is chosen to "
+                                                     "match the area of the inner mask.")
+    air_kerma = st.sidebar.text_input("Air Kerma")
+    mask_inner_fraction = float(mask_inner_fraction)
+    mask_outer_fraction = float(mask_outer_fraction)
+    st.title("IEC 62220-2 Metric Evaluation")
+    if high_data is not None and low_data is not None and len(air_kerma):
+        air_kerma = float(air_kerma)
+        high = pydicom.dcmread(high_data)
+        high_img = high.pixel_array.astype(np.float64)
+        low = pydicom.dcmread(low_data)
+        low_img = low.pixel_array.astype(np.float64)
+        if high.ImagerPixelSpacing[0] != high.ImagerPixelSpacing[1]:
+            raise RuntimeError("Anisotropic pixels not supported")
+        if low.ImagerPixelSpacing[0] != low.ImagerPixelSpacing[1]:
+            raise RuntimeError("Anisotropic pixels not supported")
+        if low.ImagerPixelSpacing[0] != high.ImagerPixelSpacing[0]:
+            raise RuntimeError("Low and high images must have the same pixel spacing")
+        if low_img.shape != high_img.shape:
+            raise RuntimeError("Low and high images must have the same shape")
+
+        height_width = ROI / np.array(high.ImagerPixelSpacing)
+        starts = ((np.array(high_img.shape) - height_width) // 2).astype(np.int)
+        stops = np.ceil(starts + height_width).astype(np.int)
+        slices = tuple((slice(s, e) for s, e in zip(starts, stops)))
+        high_img = high_img[slices]
+        low_img = low_img[slices]
+        hough_centers = hough_wrapper(high_img, high.ImagerPixelSpacing[0])
+        hough_centers = sort_circles(high_img, high.ImagerPixelSpacing[0], hough_centers)
+        if verbose:
+            st.header("High energy image with detected circle centers")
+            st.pyplot(plot_circles(high_img, hough_centers))
+        trphantom, trradius = transform_phantom(hough_centers, read_phantom(), high.ImagerPixelSpacing[0])
+        st.header("High energy image with registered ROIs")
+        st.pyplot(plot_circles(high_img, trphantom, radii=[trradius]))
+
+        rad1 = trradius * mask_inner_fraction
+        rad2 = trradius * mask_outer_fraction
+        rad3 = np.sqrt(rad1 ** 2 + rad2 ** 2)
+
+        st.header("ROI mean/std extraction")
+        st.pyplot(plot_circles(high_img, trphantom[:, :2], radii=[rad1, rad2, rad3]))
+        inner_labels, outer_labels = get_labels(high_img.shape, trphantom, rad1, rad2, rad3)
+        if verbose:
+            st.subheader("ROI Mask Visualization")
+            st.text("Masks are shown by multipling the base image by 1.1")
+            img_masks = high_img.copy()
+            img_masks[inner_labels > 0] *= 1.1
+            img_masks[outer_labels > 0] *= 1.1
+            fig, ax = plt.subplots()
+            ax.imshow(img_masks)
+            st.pyplot(fig)
+        assert(trphantom[PMMA_MID_INDEX, 2] == 6.0)
+        assert(trphantom[PMMA_MID_INDEX, 3] == 1.0)
+        assert(trphantom[AL_MID_INDEX, 2] == 1.5)
+        assert(trphantom[AL_MID_INDEX, 3] == 0.0)
+        w_al = get_w(high_img, low_img,
+                     inner_labels == PMMA_MID_INDEX + 1,
+                     outer_labels == PMMA_MID_INDEX + 1)
+        alimg = low_img / high_img ** w_al
+        w_pmma = get_w(high_img, low_img,
+                       inner_labels == AL_MID_INDEX + 1,
+                       outer_labels == AL_MID_INDEX + 1)
+        pmmaimg = low_img / high_img ** w_pmma
+
+        st.header("DE Images")
+        fig, ax = plt.subplots()
+        ax.imshow(pmmaimg)
+        ax.set_title("PMMA subtracted image")
+        st.pyplot(fig)
+        fig, ax = plt.subplots()
+        ax.imshow(alimg)
+        ax.set_title("Al subtracted image")
+        st.pyplot(fig)
+
+        st.header("DE CNR")
+        st.text("Feature indexes are separate for each material and ordered by increasing height")
+        assert(np.all(trphantom[:, 2] == np.array([0.5, 1.0, 1.5, 2.0, 2.5, 10.0, 8.0, 6.0, 4.0, 2.0])))
+        assert(np.all(trphantom[:, 3] == np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])))
+        feature_inds = np.array([1, 2, 3, 4, 5, 5, 4, 3, 2, 1])
+
+        alimg_cnr = cnr(alimg, inner_labels, outer_labels, trphantom.shape[0])
+        pmmaimg_cnr = cnr(pmmaimg, inner_labels, outer_labels, trphantom.shape[0])
+        alimg_cnr /= np.sqrt(air_kerma)
+        pmmaimg_cnr /= np.sqrt(air_kerma)
+        pmma_feature_inds = trphantom[:, 3] == 1
+        al_feature_inds = trphantom[:, 3] == 0
+
+        fig, ax = plt.subplots()
+        ax2 = ax.twinx()
+        ax.set_title("CNR / \sqrt{X} in PMMA subtracted image")
+        ax.plot(feature_inds[pmma_feature_inds], pmmaimg_cnr[pmma_feature_inds], color='b')
+        ax.set_xlabel("Feature Index")
+        ax.set_ylabel("PMMA Feature Contrast", color='b')
+        ax2.plot(feature_inds[al_feature_inds], pmmaimg_cnr[al_feature_inds], color='g')
+        ax2.set_ylabel("Al Feature Contrast", color='g')
+        st.pyplot(fig)
+
+        fig, ax = plt.subplots()
+        ax2 = ax.twinx()
+        ax.set_title("CNR / \sqrt{X} in Al subtracted image")
+        ax.plot(feature_inds[pmma_feature_inds], alimg_cnr[pmma_feature_inds], color='b')
+        ax.set_xlabel("Feature Index")
+        ax.set_ylabel("PMMA Feature Contrast", color='b')
+        ax2.plot(feature_inds[al_feature_inds], alimg_cnr[al_feature_inds], color='g')
+        ax2.set_ylabel("Al Feature Contrast", color='g')
+        st.pyplot(fig)
