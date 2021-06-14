2eaae51ced3467d4b05fa50a7683108b38ef96f0
Steven Tilley <stilley@kaimaging.com>
2021-06-14 09:51:23 -0400

    replace 'forgiving' option with try/except

--- app.py
+++ app.py
@@ -166,8 +166,11 @@ def cnr(img, inner, outer, nlabels):
     return np.array(cnr)
 
 
-def load_dcm(f, forgiving=False):
-    if forgiving:
+def load_dcm(f):
+    try:
+        dataset = pydicom.dcmread(f)
+    except (AttributeError, pydicom.errors.InvalidDicomError):
+        f.seek(0)
         pydicom.filereader.read_preamble(f, False)
         h = pydicom.filereader.read_dataset(f, is_implicit_VR=False, is_little_endian=True, stop_when=lambda tag, vr, val: tag.group > 2)
         ts = h["TransferSyntaxUID"]
@@ -184,8 +187,6 @@ def load_dcm(f, forgiving=False):
             raise RuntimeError("unsupported transfer sytanx")
         dataset = pydicom.filereader.read_dataset(f, is_implicit_VR=ivr, is_little_endian=le)
         dataset.file_meta = h
-    else:
-        dataset = pydicom.dcmread(f)
     return dataset
 
 
@@ -196,7 +197,6 @@ if __name__ == '__main__':
     matplotlib.rcParams["image.cmap"] = "Greys_r"
     high_data = st.sidebar.file_uploader("High energy image file", type=["dcm", "DCM"])
     low_data = st.sidebar.file_uploader("Low energy image file", type=["dcm", "DCM"])
-    forgiving = st.sidebar.checkbox("Read dicoms in as forgiving a manner as possible")
     quad_detrend_all = st.sidebar.checkbox("Quadratically detrend images prior to metric calculation")
     verbose = st.sidebar.checkbox("Verbose")
     mask_inner_fraction = st.sidebar.text_input("Inner ROI radius fraction",
@@ -213,9 +213,9 @@ if __name__ == '__main__':
     mask_outer_fraction = float(mask_outer_fraction)
     if high_data is not None and low_data is not None and len(air_kerma):
         air_kerma = float(air_kerma)
-        high = load_dcm(high_data, forgiving=forgiving)
+        high = load_dcm(high_data)
         high_img = high.pixel_array.astype(np.float64)
-        low = load_dcm(low_data, forgiving=forgiving)
+        low = load_dcm(low_data)
         low_img = low.pixel_array.astype(np.float64)
         if high.ImagerPixelSpacing[0] != high.ImagerPixelSpacing[1]:
             raise RuntimeError("Anisotropic pixels not supported")
